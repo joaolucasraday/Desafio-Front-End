01a881b55c1a5cca32d20b27b43fe5fa7ac1c392
João Lucas Raday <lucasraday@gmail.com>
2026-04-17 08:55:18 -0300

    mudança no weight da fonte

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -14,10 +14,11 @@ export default function Dashboard() {
     navigate("/login");
   }
 
+  
   return (
     <Layout>
       <div className="flex flex-col items-center gap-4">
-        <h1 className="text-2xl sm:text-3xl md:text-4xl font-bold text-center text-black dark:text-white">Bem vindo ao Dashboard</h1>
+        <h1 className="text-2xl sm:text-3xl md:text-4xl text-center text-black dark:text-white">Bem vindo ao Dashboard</h1>
         <Button variant="danger" onClick={handleLogout}>Sair do Sistema</Button>
       </div>
     </Layout>
